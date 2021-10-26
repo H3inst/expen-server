321260a2b3f27ce8a3f9ff613625ca7dc4669cc8
yesid <yesid.manjarres@aule.co>
2021-10-26 11:00:37 -0500

    registers table

--- database/database.sql
+++ database/database.sql
@@ -6,3 +6,11 @@ CREATE TABLE users (
     user_email TEXT NOT NULL UNIQUE,
     user_password TEXT NOT NULL
 );
+
+CREATE TABLE registers (
+    register_id VARCHAR (255) PRIMARY KEY NOT NULL,
+    register_desc TEXT NOT NULL,
+    register_ammount FLOAT NOT NULL,
+    register_category TEXT,
+    register_date TIMESTAMP
+);
