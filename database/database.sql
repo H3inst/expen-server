58e6742015465e594eb94e597da2836be611ef3e
yesid <yesid.manjarres@aule.co>
2021-11-02 10:27:17 -0500

    sql script updated

--- database/database.sql
+++ database/database.sql
@@ -4,17 +4,19 @@ CREATE TABLE users (
     user_id VARCHAR(255) PRIMARY KEY NOT NULL,
     user_name TEXT NOT NULL,
     user_email TEXT NOT NULL UNIQUE,
-    user_password TEXT NOT NULL
+    user_password TEXT NOT NULL,
+    user_balance INT NOT NULL
 );
 
 CREATE TABLE activities (
     activity_id VARCHAR (255) PRIMARY KEY NOT NULL,
+    user_id VARCHAR(255),
     activity_type VARCHAR (255) NOT NULL,
     activity_desc TEXT NOT NULL,
     activity_ammount FLOAT NOT NULL,
     activity_category TEXT,
     activity_date TIMESTAMP,
-    CONSTRAINT fk_user
+    CONSTRAINT fk_user_id
         FOREIGN KEY(user_id)
             REFERENCES users(user_id)
 );
