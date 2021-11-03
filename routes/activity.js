e5f5155fec1a8953fa4e2ce74baa2ef3d6a9c0c8
siddev <ewright.develop@gmail.com>
2021-11-02 22:13:56 -0500

    aa

--- routes/activity.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express");
-const { getUserBalance } = require("../controllers/activities_controller");
-const router = Router();
-
-router.post("/get-balance", getUserBalance);
-
-module.exports = router;
